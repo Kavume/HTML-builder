494d3b0c81ac0cc038645f68fa175f3aafc4e126
Kavume <e.gorbacheva404@gmail.com>
2022-11-04 17:16:05 +0100

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+let fs = require('fs').promises;
 
+let path = require('path');
+
+(async function copyFolder() {
+
+    const directory = "./04-copy-directory/files-copy";
+
+    try {
+        for (const file of await fs.readdir(directory)) {
+
+            await fs.unlink(path.join(directory, file));
+        }
+    } catch (err){}
+
+    fs.mkdir('./04-copy-directory/files-copy', {recursive: true});
+
+    let files = await fs.readdir('./04-copy-directory/files');
+
+    files.forEach( (item, num) => {
+        fs.copyFile( path.join('./04-copy-directory/files', files[num]), path.join('./04-copy-directory/files-copy', files[num]));
+
+    })
+
+})();
